781e6868bbe43a337ea1b80cb510004c16fc87e7
JarvinV4 <jarvinpoke@gmail.com>
2023-11-25 17:25:14 -0600

    Update

--- Empleados/Administrador.py
+++ Empleados/Administrador.py
@@ -4,8 +4,7 @@ class Administrador(Personal):
     def __init__(self, nombre, apellido, identidad, telefono, no_empleado, salario):
         super().__init__(nombre, apellido, identidad, telefono, no_empleado, salario)
 
-    def ContratarPersonal():
-        print("Contratando gente")
+    
 
     def CalculoSalario(self):
         return super().CalculoSalario()
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from Persona import Persona
 from Personal import Personal
 from Cliente import Cliente
-from Empleados import Vendedor
-from Empleados import Comprador
-from Empleados import Consultor
+from Empleados.Administrador import Administrador
+from Empleados.Comprador import Comprador
+from Empleados.Consultor import Consultor
+from Empleados.Vendedor import Vendedor
 
 
 
@@ -11,45 +12,50 @@ print("Dios mio ")
 
 
 def agregar_cliente():
-    nombre = input("Ingrese el nombre del cliente: ")
-    apellido = input("Ingrese el apellido del cliente: ")
-    identidad = input("Ingrese la identidad del cliente: ")
-    telefono = input("Ingrese el telefono del cliente: ")
-    rtn = input("Ingrese el rtn del cliente: ")
+    try:
+        nombre = input("Ingrese el nombre del cliente: ")
+        apellido = input("Ingrese el apellido del cliente: ")
+        identidad = input("Ingrese la identidad del cliente: ")
+        telefono = input("Ingrese el telefono del cliente: ")
+        rtn = input("Ingrese el rtn del cliente: ")
 
-    cliente = Cliente(nombre, apellido, identidad, telefono, rtn)
-    clientes.append(cliente)
-
+        cliente = Cliente(nombre, apellido, identidad, telefono, rtn)
+        clientes.append(cliente)
+    except:
+        print("Ingrese un valor correcto")
 
 
 def ContratarPersonal():
-    nombre = input("Ingrese el nombre del empleado: ")
-    apellido = input("Ingrese el apellido del empleado: ")
-    identidad = input("Ingrese la identidad del empleado: ")
-    telefono = input("Ingrese el telefono del empleado: ")
-    no_empleado = input("Ingrese el numero de empleado: ")
-    salario = float(input("Ingrese el salario del empleadp: "))
-    print("¿Cual sera el cargo de este empleado?")
-    print("1. Comprador")
-    print("2. Consultor")
-    print("3. Vendedor")
-    opcion=input("Seleccione una opcion ")
-    while True:
+    try:
+        nombre = input("Ingrese el nombre del empleado: ")
+        apellido = input("Ingrese el apellido del empleado: ")
+        identidad = input("Ingrese la identidad del empleado: ")
+        telefono = input("Ingrese el telefono del empleado: ")
+        no_empleado = input("Ingrese el numero de empleado: ")
+        salario = float(input("Ingrese el salario del empleadp: "))
+        print("¿Cual sera el cargo de este empleado?")
+        print("1. Comprador")
+        print("2. Consultor")
+        print("3. Vendedor")
+        opcion=input("Seleccione una opcion ")
+        while True:
 
-        if opcion =="1":
-            empleado = Comprador(nombre, apellido, identidad, telefono, no_empleado, salario)
-            empleados.append(empleado)
-            break
-        elif opcion =="2":
-            empleado = Consultor(nombre, apellido, identidad, telefono, no_empleado,salario, None)
-            empleados.append(empleado)
-            break
-        elif opcion =="3":
-            empleado = Vendedor(nombre, apellido, identidad, telefono, no_empleado, salario, None)
-            empleados.append(empleado)
-            break
-        else:
-            print("Seleccione una de las opciones disponibles")
+            if opcion =="1":
+                empleado = Comprador(nombre, apellido, identidad, telefono, no_empleado, salario)
+                empleados.append(empleado)
+                break
+            elif opcion =="2":
+                empleado = Consultor(nombre, apellido, identidad, telefono, no_empleado,salario, None)
+                empleados.append(empleado)
+                break
+            elif opcion =="3":
+                empleado = Vendedor(nombre, apellido, identidad, telefono, no_empleado, salario, None)
+                empleados.append(empleado)
+                break
+            else:
+                print("Seleccione una de las opciones disponibles")
+    except:
+        print("Ingrese un valor numerico para el salario")
 
 clientes =[]
 empleados =[]
@@ -68,10 +74,10 @@ while True:
 
     if opcion == "1":
         cliente_nuevo = agregar_cliente()
-        print("Cliente agregado")
+        
     elif opcion == "2":
         empleado_nuevo = ContratarPersonal()
-        print("Empleado agregado:")
+        
 
     elif opcion == "3":
         print("Adio")
