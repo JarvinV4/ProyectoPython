a61a840c1b3c402f4e32d7ea5d236412b6a4dd26
JarvinV4 <jarvinpoke@gmail.com>
2023-11-25 13:45:50 -0600

    Update

--- Personal.py
+++ Personal.py
@@ -1,6 +1,12 @@
 from Persona import Persona
+from abc import abstractmethod
+
 class Personal(Persona):
     def __init__(self, nombre, apellido, identidad, telefono, no_empleado, salario):
         super().__init__(nombre, apellido, identidad, telefono)
         self.no_empleado = no_empleado
         self.salario = salario
+    
+    @abstractmethod
+    def CalculoSalario(self):
+        pass
