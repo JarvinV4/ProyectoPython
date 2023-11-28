955f4bb214676abd70bbb84e0e86be1ab801bfff
Sahid Pineda <nelsonsahid504@gmail.com>
2023-11-27 19:41:39 -0600

    Update

--- Persona.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC,abstractmethod
-class Persona(ABC):
-    def __init__(self, nombre, apellido, identidad, telefono):
-        self.nombre = nombre
-        self.apellido = apellido
-        self.identidad = identidad
-        self.telefono = telefono
--- Productos/TarjetasGraficas.py
+++
@@ -1,8 +0,0 @@
-from Producto import Producto
-
-class TarjetaGrafica(Producto):
-    def __init__(self, codigo_producto, modelo, marca, fabricante, numero_de_serie, precio, cantidad_en_inventario, fecha_de_compra, fecha_de_fabricacion, proveedor, vram, salida, frecuencia_gpu):
-        super().__init__(codigo_producto, modelo, marca, fabricante, numero_de_serie, precio, cantidad_en_inventario, fecha_de_compra, fecha_de_fabricacion, proveedor)
-        self._vram = vram
-        self._salida = salida
-        self._frecuencia_gpu = frecuencia_gpu
