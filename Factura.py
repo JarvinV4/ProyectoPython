f62859487e51c56c8b639938a5c9f735b8cd7405
Eduardo-332 <allanedu333@hotmail.com>
2023-11-28 01:17:43 -0600

    ...

--- Factura.py
+++ Factura.py
@@ -33,21 +33,25 @@ class Factura():
         self.codigoFactura = codigoFactura
         
     def imprimirFactura(self):
-        print("=" * 50)
-        print("{:^30}".format("Factura"))
-        print("=" * 50)
-        print("\nNúmero de Factura: {:>30}".format(self.codigoFactura))
-        print("Fecha de Emisión: {:>44}".format(self.fecha_emision.strftime("%Y-%m-%d %H:%M:%S")))
-        print("Cliente: {:>35}".format(str(self.cliente)))
-        print("Vendedor: {:>10}".format(str(self.vendedor.nombre)),(str(self.vendedor.apellido)))
+        print("=" * 105)
+        print("{:^85}".format("Factura"))
+        print("=" * 105)
+        print("\nNúmero de Factura: {:>81}".format(self.codigoFactura))
+        print("Fecha de Emisión: {:>82}".format(self.fecha_emision.strftime("%Y-%m-%d %H:%M:%S")))
+        print("")
+        print("{:>35}".format(str(self.cliente)))
+        print("Vendedor: {:>3}".format(str(self.vendedor.nombre)),(str(self.vendedor.apellido)))
         print("\nProductos:")
         for producto in self.productosPorComprar:
             if isinstance(producto, Producto):
                 print("{:<30} {:>20}".format(f"- {str(producto.nombre_producto)}:", f"{str(producto._precio)}"))
             else:
                 print("Error: El objeto producto no es una instancia de la clase Producto.")
-        print("\nSubtotal: {:>29}".format(f"{self.subtotal}%"))
-        print("Total a Pagar: {:>38}".format(self.total))
-        print("=" * 50)
+        print("\nEstado de envio: {:>84}".format(self.control_envio))       
+        print("\nSubtotal: {:>91}".format(f"{self.subtotal}%"))
+        print("\nISV: {:>96}".format(f"{self.ISV}%"))
+        print("Total a Pagar: {:>86}".format(self.total))
+        print("=" * 105)
+
     
     
